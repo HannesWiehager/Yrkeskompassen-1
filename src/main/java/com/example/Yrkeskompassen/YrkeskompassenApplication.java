3740a181406d378f040959c9b48bd43b4459bca5
hanneswiehager <hannes.wiehager@gmail.com>
2022-10-10 13:27:03 +0200

    initial commit

---
+++ src/main/java/com/example/Yrkeskompassen/YrkeskompassenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Yrkeskompassen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YrkeskompassenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YrkeskompassenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Yrkeskompassen/YrkeskompassenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Yrkeskompassen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YrkeskompassenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
